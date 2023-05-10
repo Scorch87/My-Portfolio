fd4bc5cb39b79ee96c44c3b69e96cec5d31cdaf5
Scorch87 <44007402+Scorch87@users.noreply.github.com>
2023-05-09 20:39:27 -0500

    Update Portfolio.js

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -23,22 +23,57 @@ export default function Portfolio() {
       </h1>
       <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
       <div>
-        <img className="h-auto max-w-full rounded-lg" src={portfolio1} alt="Progressive Web Application Text Editor"/>
+        <h2>A Progressive Web Application: Text Editor</h2>
+        <a href = "https://ancient-eyrie-07043.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+          <img className="h-auto max-w-full rounded-lg" src={portfolio1} alt="Progressive Web Application Text Editor"/>
+        </a>
+        <a href = "https://github.com/Scorch87/Web-Based-Text-Editor" target="_blank" rel="noopener noreferrer">
+          View on GitHub
+        </a>
     </div>
     <div>
+    <h2>AndrewsList: A Home Improvement Marketplace</h2>
+      <a href = "https://andrews-list.herokuapp.com/" target="_blank" rel="noopener noreferrer">
         <img className="h-auto max-w-full rounded-lg" src={portfolio2} alt="Andrew's List"/>
+      </a>
+      <a href = "https://github.com/AMess33/AndrewsList" target="_blank" rel="noopener noreferrer">
+          View on GitHub
+      </a>
     </div>
     <div>
+    <h2>NoSQL API Implementation: Social Media API</h2>
+      <a href = "https://drive.google.com/file/d/15xoKITLN8gTCD2bxIBSBaca0NKN-2iPp/view" target="_blank" rel="noopener noreferrer">
         <img className="h-auto max-w-full rounded-lg" src={portfolio3} alt="Social Media API Demonstration"/>
+      </a>
+      <a href = "https://github.com/Scorch87/Social-Media-API" target="_blank" rel="noopener noreferrer">
+          View on GitHub
+      </a>
     </div>
     <div>
+        <h2>ExpressJS Back End: Note Taker</h2>
         <img className="h-auto max-w-full rounded-lg" src={portfolio4} alt="Note-Taker Application"/>
+      
+      <a href = "https://github.com/Scorch87/Note-Taker" target="_blank" rel="noopener noreferrer">
+          View on GitHub
+      </a>
     </div>
     <div>
+      <h2>Third-Party API Use- Weather Dashboard</h2>
+      <a href = "https://scorch87.github.io/Weather-Dashboard/" target="_blank" rel="noopener noreferrer">
         <img className="h-auto max-w-full rounded-lg" src={portfolio5} alt="Weather Dashboard"/>
+      </a>
+      <a href = "https://github.com/Scorch87/Weather-Dashboard" target="_blank" rel="noopener noreferrer">
+          View on GitHub
+      </a>
     </div>
     <div>
-        <img className="h-full max-w-full rounded-lg" src={portfolio6} alt="README Generator"/>
+      <h2>Command Line Interface Application: README Generator</h2>
+      <a href = "https://drive.google.com/file/d/1hF6dGCxO6xh7wKsWTE48fBp3XXuVLbM-/view" target="_blank" rel="noopener noreferrer">
+        <img className="h-80% max-w-full rounded-lg" src={portfolio6} alt="README Generator"/>
+      </a>
+      <a href = "https://github.com/Scorch87/Readme-Generator" target="_blank" rel="noopener noreferrer">
+          View on GitHub
+      </a>
     </div>
       </div>
     </div>
